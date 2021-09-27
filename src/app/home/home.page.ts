f7823e89f6cfc8e2e8d059d12f11cc79db2f35fd
Felix Rimkus <felix.rimkus@run-e.com>
2021-09-27 13:19:25 +0200

    Update home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,46 @@
 import { Component } from '@angular/core';
-import { ModalControllerMock } from 'ionic-mocks';
 
 import { DemoModalBasicComponent } from '../../demos/demo-modal-basic';
+
+function createSpyObj(_baseName: string, methodNames: string[]) {
+  const obj: any = {};
+  for (let i = 0; i < methodNames.length; i++) {
+    obj[methodNames[i]] = () => {};
+  }
+  return obj;
+}
+
+class ModalControllerMock {
+  public static instance(modalMock?: ModalMock): any {
+    const instance = createSpyObj("ModalController", ["create"]);
+    instance.create.and.returnValue(modalMock || ModalMock.instance());
+
+    return instance;
+  }
+}
+
+class ModalMock {
+  public static instance(): any {
+    let _dismissCallback: Function;
+    const instance = createSpyObj("Modal", [
+      "present",
+      "dismiss",
+      "onDidDismiss",
+    ]);
+    instance.present.and.returnValue(Promise.resolve());
+
+    instance.dismiss.and.callFake(() => {
+      _dismissCallback();
+      return Promise.resolve();
+    });
+
+    instance.onDidDismiss.and.callFake((callback: Function) => {
+      _dismissCallback = callback;
+    });
+
+    return instance;
+  }
+}
 
 @Component({
   selector: 'app-home',
